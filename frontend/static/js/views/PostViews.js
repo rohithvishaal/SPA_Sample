be523da7e6876951c095a6ae5b4334fd7d263c7c
rohith.vishaal <rohith.vishaal@gmail.com>
2022-12-27 22:50:45 +0500

    Update: Matches urls like /posts/:id

--- frontend/static/js/views/PostViews.js
+++ frontend/static/js/views/PostViews.js
@@ -3,17 +3,15 @@ import AbstractView  from "./AbstractView.js";
 export default class extends AbstractView{
     constructor(params){
         super(params)
-        this.setTitile("Dashboard")
+        this.setTitile("Viewing Post")
     }
 
     async getHtml(){
+        console.log(this.params.id)
         return `
-            <h1>Welcome back, Dom</h1>
+            <h1>you are Viewing Posts</h1>
             <p>
                 Fugiat voluptate et nisi Lorem cillum anim sit do eiusmod occaecat irure do. Reprehenderit anim fugiat sint exercitation consequat. Sit anim laborum sit amet Lorem adipisicing ullamco duis. Anim in do magna ea pariatur et.
-            </p>
-            <p>
-                <a href="/posts" data-link>View recent posts</a>.
             </p>
         `;
     }
